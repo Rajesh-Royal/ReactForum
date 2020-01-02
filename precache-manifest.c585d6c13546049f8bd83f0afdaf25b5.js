25fa1cf487ecd967d53443b5c9336f6ae4d86477
Rajesh-Royal <rajeshroyal896@gmail.com>
2020-01-02 15:51:38 +0530

    forum uploaded

---
+++ precache-manifest.c585d6c13546049f8bd83f0afdaf25b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e7104709d6d040e746e89af18f30ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8cad708644ca9d510ba",
+    "url": "/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "cd5c1017acd9beebb2fb",
+    "url": "/static/css/main.c3f1eb08.chunk.css"
+  },
+  {
+    "revision": "c8cad708644ca9d510ba",
+    "url": "/static/js/2.e81d709a.chunk.js"
+  },
+  {
+    "revision": "cd5c1017acd9beebb2fb",
+    "url": "/static/js/main.40f5106c.chunk.js"
+  },
+  {
+    "revision": "041b9356a6b68ed02624",
+    "url": "/static/js/runtime~main.1c463401.js"
+  },
+  {
+    "revision": "4f995fa49446998983e05df9994dc96c",
+    "url": "/static/media/AvenirLTStd-Medium.4f995fa4.otf"
+  }
+]);
